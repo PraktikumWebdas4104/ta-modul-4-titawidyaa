9b0e4d39fca24ba9ee8f8b021ce4d482599ad208
titawidyaa <43517924+titawidyaa@users.noreply.github.com>
2018-09-30 13:13:06 +0700

    Update formawal.php
    NAMA : TITA WIDYA KUSUMA 
    NIM : 6701174008
    KELAS : D3MI_41_04

--- formawal.php
+++ formawal.php
@@ -1,5 +1,25 @@
-<?php
+<body bgcolor="#ff66cc" align="center">
+<marquee><h1>MENU LOGIN</h1></marquee>
+<hr>
+<form action="#" method="post">  
+  <b>Nama : <input type="text" name="nama"><br/><br/>
+  NIM : <input type="password" name="nim"><br/><br/></b>
+  <input type="submit" name="login" value="Login">
+</form>
+</body>
 
+<?php 
+if (isset($_POST['login'])) {
+  $nama=$_POST['nama'];
+  $nim=$_POST['nim'];
 
-
+  if ($nama=="admin"&&$nim=="admin") {
+    echo "LOGIN SUKSES";
+?>
+  <a href="proses.php"> LINK KE PROSES </a>
+<?php 
+  }else{
+    echo "LOGIN GAGAL";
+  }
+}
 ?>
